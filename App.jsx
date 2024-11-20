feb97865d5b4929d12dcb91e5628dde3c501c36d
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-11-21 01:23:00 +0500

    initial commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Text>Hello World</Text>
+      <Text>This is a React Native App</Text>
+    </View>
+  );
+};
+
+export default App;
